be3878c1fcc46cce9b9de7533474de589277280a
leeshi <lishi.mv@gmail.com>
2019-05-14 23:26:54 +0800

    parse poetry json

--- app/src/main/java/com/lishi/adruino/randompoetry/model/RecommendationsCrawlerImpl.java
+++ app/src/main/java/com/lishi/adruino/randompoetry/model/RecommendationsCrawlerImpl.java
@@ -1,7 +1,11 @@
 package com.lishi.adruino.randompoetry.model;
 
+import com.google.gson.Gson;
 import com.lishi.adruino.randompoetry.item.PoetryItem;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -12,30 +16,27 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class RecommendationsCrawlerImpl implements Crawler{
-    private final String tarUrl = "https://www.gushiwen.org/";
+    private final String tarUrl = "https://api.gushi.ci/all.json";
 
     @Override
     public void search(OnLoadListener onLoadListener) {
         new Thread(()->{
             System.out.println();
             try{
-                Connection conn = Jsoup.connect(tarUrl).header("user-agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36");
+                Connection conn = Jsoup.connect(tarUrl).ignoreContentType(true).header("user-agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36");
                 Document doc = Jsoup.parse(conn.get().html());
+
                 ArrayList<PoetryItem> listData = new ArrayList<>();
 
-                Elements sons = doc.getElementsByClass("sons");
+                JSONObject json = new JSONObject(doc.text());
 
-                for(int i = 0;i < 10;i++){
-                    Element son = sons.get(i);
-                    Element contson = son.getElementsByClass("contson").get(0);
-                    Element source = son.getElementsByAttributeValue("class","source").get(0);
-                    Element title = son.getElementsByTag("b").get(0);
-                    String AfterRe = contson.html().replaceAll("<br>","").replaceAll("</?[^>]+>","").replaceAll("\\(.*?\\)","");
-                    listData.add(new PoetryItem(AfterRe,title.text(),source.text()));
-                }
+                listData.add(new PoetryItem(json.getString("content"),json.getString("origin"),json.getString("author")));
 
                 onLoadListener.loadSuccess(listData);
             }catch (IOException e){
+                e.printStackTrace();
+                onLoadListener.loadFailed();
+            } catch (JSONException e) {
                 e.printStackTrace();
                 onLoadListener.loadFailed();
             }
